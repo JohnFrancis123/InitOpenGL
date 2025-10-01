0afc8f72eff7cb21368cb9a5fce4c15f86f7d68c
johnF <franci24@sheridancollege.ca>
2025-10-01 13:09:05 -0400

    Task 1 complete: Rendered a textured square with wood texture

--- OpenGL/Shader.h
+++ OpenGL/Shader.h
@@ -14,6 +14,8 @@ public:
 	GLuint GetProgramID() { return m_programID; }
 	GLuint GetAttrVertices() { return m_attrVertices; }
 	GLuint GetAttrColors() { return m_attrColors; }
+	GLuint GetAttrTexCoord() { return m_attrTexCoords; }
+	GLuint GetSampler1() { return m_sampler1; }
 	GLuint GetAttrWVP() { return m_attrWVP; }
 	// Methods
 	void LoadShaders(const char* _vertexFilePath, const char* _fragmentFilePath);
@@ -31,7 +33,9 @@ private:
 	GLuint m_programID;
 	GLuint m_attrVertices;
 	GLuint m_attrColors;
+	GLuint m_attrTexCoords;
 	GLuint m_attrWVP;
+	GLuint m_sampler1;
 	GLint m_result = GL_FALSE;
 	int m_infoLogLength;
 };
