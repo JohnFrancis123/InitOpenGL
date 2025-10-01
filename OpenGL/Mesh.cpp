8b87e26dc9e18b4e8a7d9dc34c728508d842c543
johnF <franci24@sheridancollege.ca>
2025-10-01 15:00:45 -0400

    I mean task 3.

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -105,7 +105,7 @@ void Mesh::Render(glm::mat4 _wvp) {
 	glUniform1i(m_shader->GetSampler2(), 1);
 	
 	//glBindTexture(GL_TEXTURE_2D, m_texture.GetTexture());
-	//glBindTexture(GL_TEXTURE_2D, m_texture2.GetTexture());
+	//glBindTexture(GL_TEXTURE_2D, m_texture2.GetTexture()); //
 
 	//glDrawArrays(GL_TRIANGLES, 0, m_vertexData.size() / 7); // Draw the triangle
 	glDrawElements(GL_TRIANGLES, m_indexData.size(), GL_UNSIGNED_BYTE, (void*)0);
