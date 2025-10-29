50871d197ffd100ac1d43d3bbb3a467be790c120
johnF <franci24@sheridancollege.ca>
2025-10-29 15:58:18 -0400

    Implemented spotlight on wall of cubes

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -18,7 +18,7 @@ void GameController::Initialize() {
 	GLFWwindow* window = WindowController::GetInstance().GetWindow(); // Call this first, as it creates a window required by GLEW
 	M_ASSERT(glewInit() == GLEW_OK, "Failed to initialize GLEW."); // Initialize GLEW
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE); // Ensure we can capture the escape key
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Black background
+	glClearColor(0.1f, 0.1f, 0.1f, 0.1f); // Grey background
 
 	//glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
@@ -44,18 +44,20 @@ void GameController::RunGame() {
 	// Create meshes
 	m_meshLight = Mesh(); //re-initialize m_mesh to ensure it's a fresh object
 	m_meshLight.Create(&m_shaderColor); //creating the mesh, which sets up its vertex buffer and data
-	m_meshLight.SetPosition({ 1.0f, -0.5f, 0.5f });
+	m_meshLight.SetPosition({ 0.5f, 0.0f, -0.5f });
 	m_meshLight.SetScale({ 0.1f, 0.1f, 0.1f });
 
-	for (int count = 0; count < 10; count++) {
-		Mesh box = Mesh();
-		box.Create(&m_shaderDiffuse);
-		box.SetLightColor({ 1.0f, 1.0f, 1.0f });
-		box.SetLightPosition(m_meshLight.GetPosition());
-		box.SetCameraPosition(m_camera.GetPosition());
-		box.SetScale({ 0.3f, 0.3f, 0.3f });
-		box.SetPosition({ glm::linearRand(-1.0f, 1.0f), glm::linearRand(-1.0f, 1.0f), glm::linearRand(-1.0f, 1.0f) });
-		m_meshBoxes.push_back(box);
+	for (int col = 0; col < 10; col++) {
+		for (int count = 0; count < 10; count++) {
+			Mesh box = Mesh();
+			box.Create(&m_shaderDiffuse);
+			box.SetLightColor({ 1.0f, 1.0f, 1.0f });
+			box.SetLightPosition(m_meshLight.GetPosition());
+			box.SetCameraPosition(m_camera.GetPosition());
+			box.SetScale({ 0.1f, 0.1f, 0.1f });
+			box.SetPosition({ 0.0f, -0.5f + (float)count / 10.0f, -0.2f + (float)col / 10.0f });
+			m_meshBoxes.push_back(box);
+		}
 	}
 
 	//m_meshBox = Mesh();
