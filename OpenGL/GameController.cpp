c72dcd448be6fc1a5846379081955d4c875c7dbc
johnF <franci24@sheridancollege.ca>
2025-11-11 16:13:56 -0500

    Output 6: Frontface culling implemented

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -24,6 +24,9 @@ void GameController::Initialize() {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
 	srand(time(0));
 	// Create a default perspective camera
 	m_camera = Camera(WindowController::GetInstance().GetResolution());
@@ -55,12 +58,12 @@ void GameController::RunGame() {
 	m.SetScale({ 0.01f, 0.01f, 0.01f });
 	Mesh::Lights.push_back(m);
 
-	Mesh teapot = Mesh();
-	teapot.Create(&m_shaderDiffuse, "../Assets/Models/teapot.obj");
-	teapot.SetCameraPosition(m_camera.GetPosition());
-	teapot.SetScale({ 0.02f, 0.02f, 0.02f });
-	teapot.SetPosition({ 0.0f, 0.0f, 0.0f });
-	m_meshBoxes.push_back(teapot);
+	//Mesh teapot = Mesh();
+	//teapot.Create(&m_shaderDiffuse, "../Assets/Models/teapot.obj");
+	//teapot.SetCameraPosition(m_camera.GetPosition());
+	//teapot.SetScale({ 0.02f, 0.02f, 0.02f });
+	//teapot.SetPosition({ 0.0f, 0.0f, 0.0f });
+	//m_meshBoxes.push_back(teapot);
 
 	Mesh box = Mesh();
 	box.Create(&m_shaderDiffuse, "../Assets/Models/Cube.obj");
@@ -69,19 +72,19 @@ void GameController::RunGame() {
 	box.SetPosition({ -1.0f, -1.0f, -1.0f });
 	m_meshBoxes.push_back(box);
 
-	Mesh plane = Mesh();
-	plane.Create(&m_shaderDiffuse, "../Assets/Models/Plane.obj");
-	plane.SetCameraPosition(m_camera.GetPosition());
-	plane.SetScale({ 0.3f, 0.3f, 0.3f });
-	plane.SetPosition({ 0.0f, 0.0f, -1.0f });
-	m_meshBoxes.push_back(plane);
+	//Mesh plane = Mesh();
+	//plane.Create(&m_shaderDiffuse, "../Assets/Models/Plane.obj");
+	//plane.SetCameraPosition(m_camera.GetPosition());
+	//plane.SetScale({ 0.3f, 0.3f, 0.3f });
+	//plane.SetPosition({ 0.0f, 0.0f, -1.0f });
+	//m_meshBoxes.push_back(plane);
 
-	Mesh window = Mesh();
-	window.Create(&m_shaderDiffuse, "../Assets/Models/Window.obj");
-	window.SetCameraPosition(m_camera.GetPosition());
-	window.SetScale({ 0.1f, 0.1f, 0.1f });
-	window.SetPosition({ 0.0f, 0.0f, 0.0f });
-	m_meshBoxes.push_back(window);
+	//Mesh window = Mesh();
+	//window.Create(&m_shaderDiffuse, "../Assets/Models/Window.obj");
+	//window.SetCameraPosition(m_camera.GetPosition());
+	//window.SetScale({ 0.1f, 0.1f, 0.1f });
+	//window.SetPosition({ 0.0f, 0.0f, 0.0f });
+	//m_meshBoxes.push_back(window);
 #pragma endregion CreateMeshes
 
 	//Fonts f = Fonts();
