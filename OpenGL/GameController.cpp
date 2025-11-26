a4306d0ca51b01129f2e2af8f3b8cab13c379641
John Francis <franci24@sheridancollege.ca>
2025-11-26 14:30:47 -0500

    FrabeBuffers output 1 complete

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -129,7 +129,7 @@ void GameController::RunGame() {
 		}
 		m_postProcessor.End();
 
-		f.RenderText(fpsS, 100, 400, 0.5f, { 1.0f, 1.0f, 0.0f });
+		f.RenderText(fpsS, 100, 300, 0.5f, { 1.0f, 1.0f, 0.0f });
 		//f.RenderText("HELLO", 100, 100, 0.5f, {1.0f, 1.0f, 0.0f});
 
 		//f.RenderText("Testing Text", 10, 500, 0.5f, { 1.0f, 1.0f, 0.0f });
